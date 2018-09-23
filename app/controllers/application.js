6798a12d590f64ce9ff526ef4f2ab5e5c92f563b
Milo Broerse <milo.broerse@gmail.com>
2018-09-23 14:37:04 +0200

    computed board

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,36 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+
 
 export default Controller.extend({
   queryParams: ['fen'],
-  fen: null
+  fen: null,
+
+  board: computed(function() {
+    var b =[];
+    for(var i = 0; i < 4; i ++){
+      b.push('tile white');
+      b.push('tile black');
+      b.push('tile white');
+      b.push('tile black');
+      b.push('tile white');
+      b.push('tile black');
+      b.push('tile white');
+      b.push('tile black');
+
+      b.push('tile black');
+      b.push('tile white');
+      b.push('tile black');
+      b.push('tile white');
+      b.push('tile black');
+      b.push('tile white');
+      b.push('tile black');
+      b.push('tile white');
+    }
+
+    return b;
+  })
 });
+
+
+// rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
